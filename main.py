4b09699bc63b4f81ae2861ea28ccd5dc1dc135ee
Firdavs <firdavsxudoyqulov098@gmail.com>
2023-11-24 20:31:06 +0500

    firstProject

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+explanition = {
+
+    "Boolean": "- mantiqiy qiymat",
+    "Float": "- O`nlik son",
+    "For": "- Biror amalni qayta qayta bajarish",
+    "If": "- shartlarni tekshirish operatori",
+    "Integer": "- Butn son",
+    "input": "- Foydalanuvchi tomonidan kiritilinadigan nars",
+    "copy": "- malumotni nushasini kochirib beradi",
+    "string": "- yozuv",
+    "count": "- malumot takrorlanish hisoboti"
+
+}
+
+
+
+for key, value in sorted(explanition.items()):
+  print(f'{key} {value}')
+
+
+print(45)
